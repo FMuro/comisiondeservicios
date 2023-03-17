b6fad289ce416bd42950518710450677b088de76
FMuro <fmuro@us.es>
2023-03-17 18:24:52 +0100

    working python package with CLI and update README

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 # Comisión de servicios de la Universidad de Sevilla
 
-Plantilla LaTeX para las comisiones de servicio de la Universidad de Sevilla, basada en el modelo oficial del 27-1-2021.
+Es un script de python, ejecutable desde la línea de comandos, que produce una solicitud de comisión de servicios de la Universidad de Sevilla en PDF a partir de los datos básicos. La plantilla actual está basada en el modelo oficial del 27-1-2021.
 
-Puedes bajar directamente la carpeta [`tex`](tex/), rellenar los datos necesarios en el archivo `cs.tex` y compilarlo con `pdflatex`. 
+Si lo prefieres, puedes simplemente descargar el fichero [`test.tex`](test/test.tex), modificarlo con tus datos y compilarlo sin instalar el script.
 
-También se incluye (TO-DO) una utilidad de línea de comandos que permite obtener la comisión de servicio en PDF aportando simplemente los datos necesarios. Es un script the python. Para instalar las dependencias necesarias, puedes ejecutar el siguiente comando: 
+# Instalación
 
-`pip install -r requirements.txt`
+TO-DO.
+# Instrucciones de uso
+
+Necesitas crear un fichero de texto plano con los datos de la comisión de servicio. Puedes descargar el modelo [`test.yaml`](test/test.yaml), cambiarlo de nombre y editarlo con tus datos.
+
+Para generar la solicitud de comisión de servicio en PDF tienes que ejecutar el script del siguiente modo:
+
+`cservicio test.yaml`
+
+Esto generará un PDF con el mismo nombre en el directorio de ejecución, en este caso `test.pdf`.
+
+Si el resultado no fuera el deseado, puedes obtener el archivo TeX en lugar de un PDF ejecutando:
+
+`cservicio -tex test.yaml`
+
+Esto generará un archivo TeX con el mismo nombre en el directorio de ejecución, en este caso `test.tex`. Puedes editar este archivo para intentar obtener un mejor resultado. 
+
+Si quieres compilar el TeX en un ordenador donde este script no esté instalado, tendrás que copiar el logotipo. La ruta aparece dentro del fichero TeX.
 
 ![](img/cs.png)
--- csus/generate.py
+++ csus/generate.py
@@ -1,8 +1,18 @@
 import yaml  # YAML parsing
 import sys  # Parsing arguments of the script
-import os
-import time
+import os  # interact with the operating system
+import shutil  # copy files
+import tempfile  # temporary folders and files
 from jinja2 import Environment, FileSystemLoader, Template  # Jinja template
+
+# separate user-provided options and arguments
+# only expected yaml file as argument
+opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+
+# script path
+script_path = os.path.dirname(os.path.realpath(__file__))
+print(script_path)
 
 
 def my_finalize(thing):
@@ -11,11 +21,8 @@ def my_finalize(thing):
 
 
 # Folder with data and templates and do extension
-ENV = Environment(loader=FileSystemLoader('.'), extensions=[
+ENV = Environment(loader=FileSystemLoader(script_path), extensions=[
                   'jinja2.ext.do'], finalize=my_finalize)
-
-# Loading the template file
-plantilla = ENV.get_template("template/cservicio.j2")
 
 
 def yaml_loader(filepath):
@@ -25,29 +32,29 @@ def yaml_loader(filepath):
     return data
 
 
-# read data file path
-datos = sys.argv[1]
-# output folder name
-carpeta = os.path.splitext(os.path.basename(datos))[0]+"_cservicio"
-# create output folder
-if not os.path.exists(carpeta):
-    os.makedirs(carpeta)
-
-# read config data file
-config = yaml_loader("data/config.yaml")
-# read current job data
-thiscs = yaml_loader(datos)
-# Merge the dictionaries with preference for the current job data
-config.update(thiscs)
-
-
 def funcion():
-    # Opening the output file
-    with open(carpeta+"/cservicio.tex", 'w', encoding="utf8") as file:
-        # Rendering the output from data and template
-        salida = plantilla.render(config)
-        file.write(salida)  # Creating the output file
-        file.close  # Closing the output file
-    # Compile output file
-    os.system("pdflatex -output-directory=" +
-              carpeta+" "+carpeta+"/cservicio.tex")
+    # read data file path
+    datos = args[0]
+    # file name
+    nombre = os.path.splitext(os.path.basename(datos))[0]
+    # read current job data
+    with open(datos, 'r') as file_descriptor:
+        config = yaml.load(file_descriptor, Loader=yaml.FullLoader)
+    # add script path to make template and logo available
+    config.update({"camino": str(script_path)})
+    with tempfile.TemporaryDirectory() as carpeta:
+        # Opening the output file
+        with open(carpeta+"/"+nombre+".tex", 'w', encoding="utf8") as file:
+            # Loading the template file
+            plantilla = ENV.get_template("/cservicio.j2")
+            # Rendering the output from data and template
+            salida = plantilla.render(config)
+            file.write(salida)  # Creating the output file
+            file.close  # Closing the output file
+        # Compile output file
+        if "-tex" in opts:
+            shutil.copy(carpeta+"/"+nombre+".tex", ".")
+        else:
+            os.system("pdflatex -output-directory=" +
+                      carpeta+" "+carpeta+"/"+nombre+".tex")
+            shutil.copy(carpeta+"/"+nombre+".pdf", ".")
