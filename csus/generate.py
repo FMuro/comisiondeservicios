cf0c927b731c9a8ef875263ad20d4186799ede48
FMuro <fmuro@us.es>
2023-03-19 23:33:36 +0100

    soporte para licencia de hasta 3 meses

--- csus/generate.py
+++ csus/generate.py
@@ -3,6 +3,8 @@ import sys  # Parsing arguments of the script
 import os  # interact with the operating system
 import shutil  # copy files
 import tempfile  # temporary folders and files
+from datetime import datetime  # deal with dates
+from dateutil.relativedelta import relativedelta  # date computations
 from jinja2 import Environment, FileSystemLoader, Template  # Jinja template
 
 # separate user-provided options and arguments
@@ -25,6 +27,13 @@ ENV = Environment(loader=FileSystemLoader(script_path), extensions=[
                   'jinja2.ext.do'], finalize=my_finalize)
 
 
+def datetime_format(value, format="%d-%m-%Y"):
+    return value.strftime(format)
+
+
+ENV.filters["datetime_format"] = datetime_format
+
+
 def funcion():
     # read data file path
     datos = args[0]
@@ -37,19 +46,34 @@ def funcion():
         # get job data
         if estretrabajo is not None:
             config.update(estretrabajo)
+    # get date/time as such in python
+    for key in ['ida', 'vuelta']:
+        print(config[key])
+        config[key] = datetime.strptime(config[key], "%d-%m-%Y %H:%M")
+    three_months_from_ida = config['ida'] + relativedelta(months=+3)
+    fifteen_days_from_ida = config['ida'] + relativedelta(days=+15)
+    documentos = ['cservicio']
+    if config['vuelta'] < fifteen_days_from_ida:
+        config.update({"menosde15dias": "menosde15dias"})
+        documentos.append('licencia')
+    elif config['vuelta'] < three_months_from_ida:
+        documentos.append('licencia')
+    else:
+        config.update({"licencia": "masde3meses"})
     with tempfile.TemporaryDirectory() as carpeta:
-        # Opening the output file
-        with open(carpeta+"/"+nombre+".tex", 'w', encoding="utf8") as file:
-            # Loading the template file
-            plantilla = ENV.get_template("/cservicio.j2")
-            # Rendering the output from data and template
-            salida = plantilla.render(config)
-            file.write(salida)  # Creating the output file
-            file.close  # Closing the output file
-        # Compile output file
-        if "-tex" in opts:
-            shutil.copy(carpeta+"/"+nombre+".tex", ".")
-        else:
-            os.system("pdflatex -output-directory=" +
-                      carpeta+" "+carpeta+"/"+nombre+".tex")
-            shutil.copy(carpeta+"/"+nombre+".pdf", ".")
+        for output in documentos:
+            # Opening the output file
+            with open(carpeta+"/"+nombre+"_"+output+".tex", 'w', encoding="utf8") as file:
+                # Loading the template file
+                plantilla = ENV.get_template("/"+output+".j2")
+                # Rendering the output from data and template
+                salida = plantilla.render(config)
+                file.write(salida)  # Creating the output file
+                file.close  # Closing the output file
+            # Compile output file
+            if "-tex" in opts:
+                shutil.copy(carpeta+"/"+nombre+"_"+output+".tex", ".")
+            else:
+                os.system("pdflatex -output-directory=" +
+                          carpeta+" "+carpeta+"/"+nombre+"_"+output+".tex")
+                shutil.copy(carpeta+"/"+nombre+"_"+output+".pdf", ".")
